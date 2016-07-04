033763180d3dd1fe89d3f2c73109394f8d0b9c86
Wu Zefeng <flamesdesperado@gmail.com>
2016-07-04 18:13:48 +0800

    Initial Commit

---
+++ Ant Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ant Planner
+//
+//  Created by Desperado on 7/4/28 H.
+//  Copyright © 28 Heisei Ant Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
