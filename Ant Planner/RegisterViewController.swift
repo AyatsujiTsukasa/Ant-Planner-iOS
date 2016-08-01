e0371c0250c36570a11519ea0fc666fb7281ab60
Wu Zefeng <flamesdesperado@gmail.com>
2016-08-01 12:45:21 +0800

    original
    Added login and register

--- Ant Planner/RegisterViewController.swift
+++ Ant Planner/RegisterViewController.swift
@@ -9,8 +9,62 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirm: UITextField!
 
+    func matchesForRegexInText(regex: String!, text: String!) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: [])
+            let nsString = text as NSString
+            let results = regex.matchesInString(text,
+                                                options: [], range: NSMakeRange(0, nsString.length))
+            return results.map { nsString.substringWithRange($0.range)}
+        } catch let error as NSError {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+
+    func extract(str: String) -> String {
+        return str.substringWithRange(Range<String.Index>(str.startIndex.advancedBy(4)..<str.endIndex.advancedBy(-5)))
+    }
+    
     @IBAction func register(sender: UIButton) {
+        
+        let usernameInput = username.text
+        let emailInput = email.text
+        let passwordInput = password.text
+        let confirmPasswordInput = confirm.text
+        
+        let urlStr = "https://www.antplanner.org/register.php?username=\(usernameInput!)&email=\(emailInput!)&password=\(passwordInput!)&password2=\(confirmPasswordInput!)"
+        let url = NSURL(string: urlStr)
+        print(urlStr)
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "GET"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            
+            let info = self.matchesForRegexInText("<li\\b[^>]*>(.*?)</li>", text: responseString as! String).map(self.extract)
+            print(info)
+            
+            if (self.matchesForRegexInText("Account Created", text: responseString as! String).count > 0) {
+//                Account Created.
+//                Alert ("success", info).
+            } else {
+//                Error.
+//                Alert ("Failed", info).
+            }
+            
+            //
+            
+        }
+        task.resume()
+        
         // Example of alert message when sign up fails
         /* if (...) {
             var alertView = UIAlertView()
@@ -23,6 +77,7 @@ class RegisterViewController: UIViewController {
             .....
         } */
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
